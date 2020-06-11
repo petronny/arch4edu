ddf74f42b0ba70183510aaa3d7b929cfc22c80fd
Jingbei Li <i@jingbei.li>
2020-06-11 23:41:34 +0800

    updated lilac.py

--- ccnet-server/lilac.py
+++ ccnet-server/lilac.py
@@ -7,5 +7,17 @@ repo_depends = ['libsearpc']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
+from lilac2.lilacyaml import load_lilac_yaml
+from pathlib import Path
+
+def download_repo_depends(path='/tmp'):
+    conf = load_lilac_yaml(Path('.'))
+    if 'repo_depends' in conf:
+        run_cmd()
+
 if __name__ == '__main__':
+
+    import sys
+    download_repo_depends()
+    sys.exit()
     single_main(build_prefix)
