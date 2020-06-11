8bf7ccc31783a0a531302363040936be1960fe96
Jingbei Li <i@jingbei.li>
2020-06-12 01:46:36 +0800

    download_repo_depends test

--- ccnet-server/lilac.py
+++ ccnet-server/lilac.py
@@ -7,16 +7,24 @@ repo_depends = ['libsearpc']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
-from lilac2.lilacyaml import load_lilac_yaml
 from pathlib import Path
+from lilac2.lilacyaml import load_lilac_yaml
 
-def download_repo_depends(path='/tmp'):
-    conf = load_lilac_yaml(Path('.'))
+def download_repo_depends(package=None):
+    if package:
+        conf = load_lilac_yaml(Path('..') / package)
+    else:
+        conf = load_lilac_yaml(Path('.'))
+
+    _repo_depends = []
     if 'repo_depends' in conf:
-        run_cmd()
+        repo_depends = conf['repo_depends']
+
+    for i in repo_depends:
+        run_cmd['download-package-from-repo.sh', i, 'arch4edu', 'x86_64', '~/repo_depends']
+        download_repo_depends(i)
 
 if __name__ == '__main__':
-
     import sys
     download_repo_depends()
     sys.exit()
