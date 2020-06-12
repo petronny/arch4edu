5b21c473abae14c1f8628870543086bd525be52c
Jingbei Li <i@jingbei.li>
2020-06-12 16:26:52 +0800

    external python lib test

--- ccnet-server/lilac.py
+++ ccnet-server/lilac.py
@@ -7,38 +7,8 @@ repo_depends = ['libsearpc']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
-import os
-from pathlib import Path
-from lilac2.lilacyaml import load_lilac_yaml
-
-def download_repo_depends(package=None):
-    if package:
-        path = Path('..') / package
-    else:
-        path = Path('.')
-
-    _repo_depends = []
-    try:
-        conf = load_lilac_yaml(path)
-        if 'repo_depends' in conf:
-            _repo_depends = conf['repo_depends']
-    except FileNotFoundError:
-        pass
-
-    for i in _repo_depends:
-        if type(i) is tuple:
-            pkgbase, pkgname = i
-        else:
-            pkgbase, pkgname = i, i
-
-        try:
-            run_cmd(['download-package-from-artifact.sh', 'petronny/arch4edu', pkgbase, pkgname, '~/repo_depends'])
-        except:
-            run_cmd(['download-package-from-repo.sh', pkgbase, 'arch4edu', 'x86_64', '~/repo_depends'])
-
-        download_repo_depends(i)
-
 if __name__ == '__main__':
+    from action_tools import download_repo_depends
     download_repo_depends()
 
     makechrootpkg_args = []
