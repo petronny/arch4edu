52b8e03b238e8b8bc212a3c77aeff7b8972687f7
Jingbei Li <i@jingbei.li>
2020-06-12 17:20:20 +0800

    more action_main test

---
+++ libsearpc/lilac.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
+update_on = [{'aur': None}]
+build_prefix = 'extra-x86_64'
+pre_build = aur_pre_build
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    from action_tools import action_main
+    action_main()
