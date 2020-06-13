e2ad24907a800c887225a8b15eb391098a57bae4
Jingbei Li <i@jingbei.li>
2020-06-13 23:43:26 +0800

    fixed libsearpc/lilac.py

--- libsearpc/lilac.py
+++ libsearpc/lilac.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
+import os
 from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
-build_prefix = 'extra-x86_64'
+build_prefix = os.path.expanduser('~/git/actions/action-extra-x86_64')
 pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
     from action_tools import action_main
-    action_main()
+    action_main(build_prefix)
